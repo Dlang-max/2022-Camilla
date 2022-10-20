2d4beab5480b051c9d7047ad7258abf7f9018f22
Dlang-max <langd052405@gmail.com>
2022-10-20 17:58:51 -0400

    initial commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+public class RobotMap {
+    public static final int RIGHT_FRONT_DRIVE_ID = 0;
+    public static final int RIGHT_BACK_DRIVE_ID = 0;
+    public static final int LEFT_FRONT_DRIVE_ID = 0;
+    public static final int LEFT_BACK_DRIVE_ID = 0;
+}
